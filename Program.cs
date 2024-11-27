df040cfa5a2c54d3139b109421bc53a9e514cf61
Bytomancer <bytomancer@gmail.com>
2024-11-27 00:30:03 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+
+using solutions;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Enter the problem identifier (format: d00s0)");
+        var s = Console.ReadLine() ?? "";
+        s = s.Trim();
+        var pattern = @"^d(\d\d)s(\d)$";
+        var r = new Regex(pattern);
+
+        var match = r.Match(s);
+
+        var day = "ERR";
+        var sol = "ERR";
+
+        foreach (Group cap in match.Groups.Cast<Group>())
+        {
+            if (cap.Index == 1)
+            {
+                day = cap.Value;
+            }
+            else if (cap.Index == 4)
+            {
+                sol = cap.Value;
+            }
+        }
+
+        day = day.PadLeft(2, '0');
+
+        var className = $"solutions.Day{day}";
+        var funcName = $"Solution{sol}";
+        Type type = Type.GetType(className) ?? throw new Exception($"Invalid day: {className}");
+        var func = type.GetMethod(funcName) ?? throw new Exception($"Invalid solution: {funcName}");
+        var obj = Activator.CreateInstance(type);
+        func.Invoke(obj, [""]);
+    }
+}
---
+++ solutions/d01s1.cs
@@ -0,0 +1,10 @@
+namespace solutions
+{
+    public class Day01
+    {
+        public void Solution1(String input)
+        {
+            Console.WriteLine("HOLY SHIT");
+        }
+    }
+}
